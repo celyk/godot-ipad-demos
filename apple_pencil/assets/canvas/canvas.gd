d3b713d9b19c4edc38cba690abba1bf179c764a7
celyk <humanimations10@gmail.com>
2024-11-09 19:11:33 +1100

    Show preview stroke

--- apple_pencil/assets/canvas/canvas.gd
+++ apple_pencil/assets/canvas/canvas.gd
@@ -22,27 +22,24 @@ func _input(event: InputEvent) -> void:
 func create_stroke(event: InputEvent):
 	preview_stroke = PencilStroke.new()
 	preview_stroke.begin()
-	preview_stroke.add_point(_screen_to_world(event.position))
+	preview_stroke.add_point(_screen_to_local(event.position))
+	
+	add_child(preview_stroke)
 
 func update_stroke(event: InputEventScreenDrag):
-	preview_stroke.add_point(_screen_to_world(event.position))
+	preview_stroke.add_point(_screen_to_local(event.position))
 
 func submit_stroke(event: InputEvent):
 	preview_stroke.end()
 	
+	remove_child(preview_stroke)
 	get_parent().add_child(preview_stroke)
 	strokes.append(preview_stroke)
 	
 	preview_stroke = null
 
-
-func _screen_to_world(p:Vector2) -> Vector2:
+func _screen_to_local(p:Vector2) -> Vector2:
 	return get_parent().make_canvas_position_local(p)
-	
-	#p = get_viewport().global_canvas_transform.affine_inverse() * p
-	#p = get_viewport().get_camera_2d().get_cakera
-	
-	return p
 
 func is_stylus(event: InputEvent) -> bool:
 	return not is_nan(event.pressure)
